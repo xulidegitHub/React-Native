434d7d6e07e1e7ef0077786a7e5b17d9d0e1a0c0
Li Xu <18211083324@163.com>
2026-02-28 17:05:47 +0800

    添加文件

---
+++ LoginModule/LoginModule/test.swift
@@ -0,0 +1,21 @@
+//
+//  test.swift
+//  LoginModule
+//
+//  Created by xuli on 2026/2/28.
+//
+import SwiftUI
+
+public struct LoginContentView: View {
+    public init() { }
+    
+    public var body: some View {
+        VStack {
+            Image(systemName: "gloasdfasdfbe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello,sdflslasdfas;dfklsk;aldfdf world!")
+        }
+        .padding()
+    }
+}
